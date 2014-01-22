4dfc00fa81805df6121c091f8e8b080d65e5ccd9
Marko Heijnen <info@markoheijnen.com>
2014-01-23 00:01:52 +0100

    Update reveal code

--- styles/reveal/pagetemplate.php
+++ styles/reveal/pagetemplate.php
@@ -49,34 +49,36 @@
 <?php wp_footer(); ?>
 
 		<script>
-			// Parse the query string into a key/value object
-			var query = {};
-			location.search.replace( /[A-Z0-9]+?=(\w*)/gi, function(a) {
-				query[ a.split( '=' ).shift() ] = a.split( '=' ).pop();
-			} );
+			window.onload=function(){
+				// Parse the query string into a key/value object
+				var query = {};
+				location.search.replace( /[A-Z0-9]+?=(\w*)/gi, function(a) {
+					query[ a.split( '=' ).shift() ] = a.split( '=' ).pop();
+				} );
 
-			Reveal.initialize({
-				// Display controls in the bottom right corner
-				controls: true,
+				Reveal.initialize({
+					// Display controls in the bottom right corner
+					controls: true,
 
-				// Display a presentation progress bar
-				progress: true,
+					// Display a presentation progress bar
+					progress: true,
 
-				// If true; each slide will be pushed to the browser history
-				history: true,
+					// If true; each slide will be pushed to the browser history
+					history: true,
 
-				// Flags if mouse wheel navigation should be enabled
-				mouseWheel: false,
+					// Flags if mouse wheel navigation should be enabled
+					mouseWheel: false,
 
-				// Apply a 3D roll to links on hover
-				rollingLinks: false,
+					// Apply a 3D roll to links on hover
+					rollingLinks: false,
 
-				// UI style
-				theme: query.theme || 'marko', // default/neon
+					// UI style
+					theme: query.theme || 'marko', // default/neon
 
-				// Transition style
-				transition: query.transition || 'default' // default/cube/page/concave/linear(2d)
-			});
+					// Transition style
+					transition: query.transition || 'default' // default/cube/page/concave/linear(2d)
+				});
+			};
 		</script>
 </body>
 </html>
